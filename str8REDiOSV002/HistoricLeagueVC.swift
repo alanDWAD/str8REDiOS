0f145ad1bb04c51d66578ba9cee6bee2d596a63f
alantingey <Ammo1979kmf>
2017-07-22 06:56:18 +0100

    Update to league tableviews

--- str8REDiOSV002/HistoricLeagueVC.swift
+++ str8REDiOSV002/HistoricLeagueVC.swift
@@ -1,29 +1,26 @@
 //
-//  OverallLeagueVC.swift
+//  HistoricLeagueVC.swift
 //  str8REDiOSV002
 //
-//  Created by Alan Tingey on 07/07/2017.
+//  Created by Alan Tingey on 22/07/2017.
 //  Copyright © 2017 Alan Tingey. All rights reserved.
 //
 
 import UIKit
 
-
-
-struct Player {
+struct Player3 {
     let name : String
     var score : String
 }
 
+class HistoricLeagueVC: UITableViewController {
 
-class OverallLeagueVC: UITableViewController {
-    
-  var players = [Player]()
+    var players = [Player3]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let urlString = "https://str8red.com/jsonoverallleaderboard/current/"
+        
+        let urlString = "https://str8red.com/jsonoverallleaderboard/1025/"
         
         let url = URL(string: urlString)
         URLSession.shared.dataTask(with:url!) { (data, response, error) in
@@ -32,7 +29,7 @@ class OverallLeagueVC: UITableViewController {
             } else {
                 do {
                     let parsedData = try JSONSerialization.jsonObject(with: data!) as! [[String]]
-                    self.players = parsedData.map { Player(name: $0[0], score: $0[1]) }
+                    self.players = parsedData.map { Player3(name: $0[0], score: $0[1]) }
                     print(self.players.count)
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
@@ -44,32 +41,25 @@ class OverallLeagueVC: UITableViewController {
             }
             
             }.resume()
-
+        
     }
- 
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
-
-
+    
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         // #warning Incomplete implementation, return the number of rows
-        
-        
-        if self.players.count == 0 {
-            return self.players.count
-        }
-        else {
-            return 1
-        }
+        return self.players.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -78,7 +68,7 @@ class OverallLeagueVC: UITableViewController {
         let player = self.players[indexPath.row]
         cell.textLabel?.text = player.name
         cell.detailTextLabel?.text = NumberFormatter.localizedString(from: NSNumber(value: Int(player.score)!), number: NumberFormatter.Style.decimal)
-
+        
         return cell
     }
     
@@ -86,6 +76,6 @@ class OverallLeagueVC: UITableViewController {
         return "Overall Leaderboard:"
     }
     
-
-
+    
+    
 }
